41b8af19614a5260ccf4ad27845eaed770b2e912
Aaron Sachs <aaronm.sachs@gmail.com>
2019-09-24 11:18:07 -0400

    Forgot to update SHA512

--- Formula/sensu-go.rb
+++ Formula/sensu-go.rb
@@ -3,7 +3,7 @@ class SensuGo < Formula
     desc "Sensu Go"
     homepage "https://sensu.io"
     url "https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/#{version}/sensu-go_#{version}_darwin_amd64.tar.gz
-    sha256 "5ae2725b5a48bbdecb57e3a329f73fcc3198935a2853888531cef14e6e7e68b8"
+    sha256 "c6cb79080351226e731b9ebaf0b317861770623ca685547b80600d1769b43bbaed51ed42fee7b31dcf545d02ccd0e6cc85927713d889a79392bd23c6451b3d6b"
 
     def install
         bin.install "sensuctl"
