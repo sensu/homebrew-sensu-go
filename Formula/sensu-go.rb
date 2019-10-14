844d51b07c3e9753baffabc26e9224feb6d03992
Cameron Johnston <cameron@sensu.io>
2019-10-14 14:57:25 -0600

    Fix checksum for 5.14.0

--- Formula/sensu-go.rb
+++ Formula/sensu-go.rb
@@ -3,7 +3,7 @@ class SensuGo < Formula
     desc "Sensu Go"
     homepage "https://sensu.io"
     url "https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/#{version}/sensu-go_#{version}_darwin_amd64.tar.gz"
-    sha256 "1208b3c935998a6ba4318630acb19efef97336431abf78b58ba5f34c1d574c08"
+    sha256 "0f2d7e4081c58a3f32cf2e0f74060a2d5353b914bff7c5179a78e7c68c6805e0"
 
     def install
         bin.install "sensuctl"
