a2df205790b344942a091b3ef883086959951624
Cameron Johnston <cameron@sensu.io>
2019-11-19 21:04:19 -0700

    update shasum for 5.15.0

--- Formula/sensu-go.rb
+++ Formula/sensu-go.rb
@@ -4,7 +4,7 @@ class SensuGo < Formula
     desc "Sensu Go"
     homepage "https://sensu.io"
     url "https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/#{version}/sensu-go_#{version}_darwin_amd64.tar.gz"
-    sha256 "0f2d7e4081c58a3f32cf2e0f74060a2d5353b914bff7c5179a78e7c68c6805e0"
+    sha256 "b1bc8ff4cac6e588e9d08b191f9404a1c2ae70e8597b5ab7928e3d965e22c58c"
 
     def install
         bin.install "sensuctl"
