3f5374bca7707ee6b4f33f6d7367f3d8a01dceef
Aaron Sachs <aaronm.sachs@gmail.com>
2020-05-16 07:24:47 -0400

    Update sensu-go.rb

--- Formula/sensu-go.rb
+++ Formula/sensu-go.rb
@@ -1,10 +1,10 @@
 class SensuGo < Formula
-    version '5.19.0'
+    version '5.20.1'
     version_scheme 1
     desc "Sensu Go"
     homepage "https://sensu.io"
     url "https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/#{version}/sensu-go_#{version}_darwin_amd64.tar.gz"
-    sha256 "7a124794995e06cccbf1919553b9ed4b4e87125dfe1cd051747d1ed4ce8c5665"
+    sha256 "6f3af9933f61381055c2718668adb8059ffd81d2cc2298a55b1e430287098c92"
 
     def install
         bin.install "sensuctl"
