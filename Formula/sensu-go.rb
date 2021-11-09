c419322a716bc6b58f09dfdb2a6ed0c0c00a75b1
Aaron Sachs <aaronm.sachs@gmail.com>
2021-11-09 16:01:47 -0500

    Updating to 6.5.4

--- Formula/sensu-go.rb
+++ Formula/sensu-go.rb
@@ -2,7 +2,7 @@ class SensuGo < Formula
     version = '6.5.4'
     desc "Sensu Go"
     homepage "https://sensu.io"
-    url "https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/#{version}/sensu-enterprise-go_#{version}_darwin_amd64.tar.gz"
+    url "https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/#{version}/sensu-go_#{version}_darwin_amd64.tar.gz"
     sha256 "c07f2c994d8308ff75fbc7e9653d095802320bddd05d978fdf0cf5dff32f5e4b"
 
     def install
