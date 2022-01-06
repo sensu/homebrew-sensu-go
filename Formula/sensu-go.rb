a78ab45ab4a88efc9136aae358947c5590091c04
Aaron Sachs <aaronm.sachs@gmail.com>
2022-01-06 15:38:35 -0500

    Updates formula to use 6.6.3
    Updates formula to use 6.6.3

--- Formula/sensu-go.rb
+++ Formula/sensu-go.rb
@@ -1,9 +1,9 @@
 class SensuGo < Formula
-    version = '6.5.4'
+    version = '6.6.3'
     desc "Sensu Go"
     homepage "https://sensu.io"
     url "https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/#{version}/sensu-go_#{version}_darwin_amd64.tar.gz"
-    sha256 "c07f2c994d8308ff75fbc7e9653d095802320bddd05d978fdf0cf5dff32f5e4b"
+    sha256 "7b28b23fb192daaf9c7f5a98a5ca5740ad589385152bdce5824020b3a92a48a0"
 
     def install
         bin.install "sensuctl"
