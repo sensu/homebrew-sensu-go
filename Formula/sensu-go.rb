b8edf8c0c77193c3f59aa730b8a5ce662be66905
Aaron Sachs <aaronm.sachs@gmail.com>
2022-04-26 09:09:42 -0400

    Update homebrew formula to 6.7.0

--- Formula/sensu-go.rb
+++ Formula/sensu-go.rb
@@ -1,9 +1,9 @@
 class SensuGo < Formula
-    version = '6.6.5'
+    version = '6.7.0'
     desc "Sensu Go"
     homepage "https://sensu.io"
     url "https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/#{version}/sensu-go_#{version}_darwin_amd64.tar.gz"
-    sha256 "c39e7d28f70980ab999e1d697783d181dd6fdfec93ae4f9337d388969359eeb8"
+    sha256 "23399536b064351f9ab8a9f70f207ed5485fdb2dd117aa774890336b97437d13"
 
     def install
         bin.install "sensuctl"
