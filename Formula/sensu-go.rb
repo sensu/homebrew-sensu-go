6e184843aa22193fd82be225ea74bd9092faa98f
Aaron Sachs <898627+asachs01@users.noreply.github.com>
2022-11-30 10:34:10 -0500

    Update sensu-go.rb

--- Formula/sensu-go.rb
+++ Formula/sensu-go.rb
@@ -4,6 +4,7 @@ class SensuGo < Formula
     homepage "https://sensu.io"
     url "https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/#{version}/sensu-go_#{version}_darwin_amd64.tar.gz"
     sha256 "b0090da83678a9271ffe22f98b31f39d8609652d1af08067ad2086af32140038"
+    license "MIT"
 
     def install
         bin.install "sensuctl"
