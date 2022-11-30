55e84f39b5f72b98fe835f10c025c938200397f0
Aaron Sachs <898627+asachs01@users.noreply.github.com>
2022-11-30 11:00:01 -0500

    Update sensu-go.rb

--- Formula/sensu-go.rb
+++ Formula/sensu-go.rb
@@ -5,10 +5,11 @@ class SensuGo < Formula
     url "https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/#{version}/sensu-go_#{version}_darwin_amd64.tar.gz"
     sha256 "b0090da83678a9271ffe22f98b31f39d8609652d1af08067ad2086af32140038"
     license "MIT"
-    head "https://https://github.com/sensu/homebrew-sensu-go.git"
+    head "https://github.com/asachs01/homebrew-sensu-go.git"
 
     def install
         bin.install "sensuctl"
+        bin.install "sensu-agent"
     end
     
     test do
