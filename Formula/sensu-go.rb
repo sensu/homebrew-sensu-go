2f7bd7bb68c8ca29ba9594eeb5851e5efe84f9ef
Aaron Sachs <98627+asachs01@users.noreply.github.com>
2022-12-01 14:47:58 -0500

    Updating formula to include agent & agent service
    Signed-off-by: Aaron Sachs <98627+asachs01@users.noreply.github.com>

--- Formula/sensu-go.rb
+++ Formula/sensu-go.rb
@@ -1,6 +1,6 @@
 class SensuGo < Formula
     version = '6.9.0'
-    desc "Sensu Go"
+    desc "Sensu Go command line tool and agent"
     homepage "https://sensu.io"
     url "https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/#{version}/sensu-go_#{version}_darwin_amd64.tar.gz"
     sha256 "b0090da83678a9271ffe22f98b31f39d8609652d1af08067ad2086af32140038"
@@ -10,15 +10,37 @@ class SensuGo < Formula
     def install
         bin.install "sensuctl"
         bin.install "sensu-agent"
-        (bin/"download_sensu_agent_config").write <<--EOS.undent
+
+        # Create directory for Sensu agent config
+        (etc/"sensu").mkpath
+
+        # Create a script to download an example agent config
+        (etc/"sensu"/"download_sensu_agent.sh").write <<~EOS
         #!/bin/sh
-        curl -L https://docs.sensu.io/sensu-go/latest/files/agent.yml -o /etc/sensu/agent.yml
+        curl -L https://docs.sensu.io/sensu-go/latest/files/agent.yml -o #{etc}/sensu/agent.yml
+        EOS
+
+        chmod "+x", "#{etc}/sensu/download_sensu_agent.sh"
+        system "#{etc}/sensu/download_sensu_agent.sh"
+
+        (var/"cache/sensu").mkpath
     end
-    
-    #def post_install
-        
-    
+
+#    def post_install
+#        (var/"sensu").mkpath
+#        chmod "-R", "0777", (var/"sensu")
+#    end
+
+    service do
+        run [bin/"sensu-agent", "start", "-c", etc/"sensu/agent.yml", "--cache-dir", var/"cache/sensu"]
+        keep_alive true
+        working_dir var
+        log_path var/"log/sensu-agent.log"
+        error_log_path var/"log/sensu-agent.log"
+    end
+
     test do
         system "#{bin}/sensuctl", "--help"
+        system "#{bin}/sensu-agent version"
     end
 end
